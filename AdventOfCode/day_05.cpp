4b9df5add2149ea17cd3f3963bf0e1590f64c7f2
Paul Godavari <paulgod@gmail.com>
2023-12-05 14:40:44 -0600

    Day05: adding test data and number parsing

--- AdventOfCode/day_05.cpp
+++ AdventOfCode/day_05.cpp
@@ -6,14 +6,50 @@
 
 #include "advent_of_code.h"
 
+#define TESTING 1
 
-static const char* input_file = "day_05.test_input";
-// static const char* input_file = "day_05.input";
+#ifdef TESTING
+    static const char* input_file = "day_05.test_input";
+    static const int kMapSize = 100;
+#else
+    static const char* input_file = "day_05.input";
+    static const int kMapSize = 1000000000;
+#endif
 
 
 void Day05()
 {
-    fprintf(stdout, "Day 05\n");
+    // Seed numbers
+    File input_data = ReadFile(input_file);
+    if (!input_data.handle) {
+        fprintf(stderr, "Can't read input file\n");
+        return -1;
+    }
+    
+    const char* data = input_data.data;
+    int index = 0;
+    bool end_of_line = false;
+    int number = 0;
+    while (!end_of_line) {
+        char current = data[index];
+        switch (current) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9': {
+                int digit = current - '0';
+                number = number * 10 + digit;
+            }
+            default: {
+            }
+        }
+    }
 }
 
 
