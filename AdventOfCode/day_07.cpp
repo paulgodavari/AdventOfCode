f73ea185e23dfaa0a830bddab6c271fa23b0fe15
Paul Godavari <paulgod@gmail.com>
2023-12-07 14:40:29 -0600

    Day 07: read the input data and echo it

--- AdventOfCode/day_07.cpp
+++ AdventOfCode/day_07.cpp
@@ -7,8 +7,49 @@
 #include "advent_of_code.h"
 
 
+static const char* input_file_name = "day_07.test_input";
+// static const char* input_file_name = "day_07.input";
+
+
+static const u32 kMaxGames = 1024;
+
+
+struct Game
+{
+    String hand;
+    u32 bid;
+};
+
+
 void Day07()
 {
-    fprintf(stderr, "Day 07\n");
+    File input_file = ReadFile(input_file_name);
+    if (!input_file.handle) {
+        fprintf(stderr, "Error reading: %s\n", input_file_name);
+        return;
+    }
+    
+    u32 game_count = 0;
+    Game games[kMaxGames] = {};
+    
+    ParseState parser = { input_file.data, input_file.size, 0 };
+    
+    while (!AtEndOfFile(&parser)) {
+        String hand = ParseWord(&parser);
+        Advance(&parser);  // Space
+        u32 bid = ParseNumber(&parser);
+        assert(AtEndOfLine(&parser));
+        Advance(&parser);  // End of Line
+        
+        games[game_count] = { hand, bid };
+        game_count++;
+    }
+    
+    fprintf(stdout, "Game count: %u\n", game_count);
+    for (int i = 0; i < game_count; ++i) {
+        fprintf(stdout, "Game %u: %.*s -> %u\n", i, (int) games[i].hand.size, games[i].hand.start, games[i].bid);
+    }
+    
+    CloseFile(&input_file);
 }
 
