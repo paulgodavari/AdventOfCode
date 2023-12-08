f08dad0031c1e50895a411a1a912f89ed17a4818
Paul Godavari <paulgod@gmail.com>
2023-12-08 10:20:00 -0600

    Day 08: convert strings to unique index values

--- AdventOfCode/day_08.cpp
+++ AdventOfCode/day_08.cpp
@@ -11,6 +11,29 @@ static const char* input_file_name = "day_08.test_input";  // Part 1: , Part 2:
 // static const char* input_file_name = "day_08.input";  // Part 1: , Part 2:
 
 
+static const u32 kMaxIndexValue = 26*26*26;
+
+
+struct IndexPair
+{
+    u32 left;
+    u32 right;
+};
+
+
+u32 StringToIndex(String str)
+{
+    u32 result = 0;
+    
+    for (int i = 0; i < str.size; ++i) {
+        u32 num = str.start[i] - 'A';
+        result = result * 26 + num;
+    }
+    
+    return result;
+}
+
+
 void Day08()
 {
     fprintf(stdout, "Day 08\n");
@@ -21,29 +44,41 @@ void Day08()
         return;
     }
     
+    IndexPair table[kMaxIndexValue] = {};
+    
     ParseState parser = { input_file.data, input_file.size, 0 };
     
     String directions = ParseWord(&parser);
     Advance(&parser, 2);
     
-    u32 line_count = 0;
+    u32 index_count = 0;
     while (!AtEndOfFile(&parser)) {
         String index = ParseWord(&parser);
         assert(index.size == 3);
         Advance(&parser, 4);
         String left = ParseWord(&parser);
+        assert(left.size == 3);
         Advance(&parser, 2);
         String right = ParseWord(&parser);
+        assert(right.size == 3);
         Advance(&parser);
         assert(AtEndOfLine(&parser));
         
-        fprintf(stdout, "%u: %.*s -> [%.*s, %.*s]\n",
-                line_count,
+        u32 i = StringToIndex(index);
+        IndexPair p = { StringToIndex(left), StringToIndex(right) };
+        assert(table[i].left == 0);
+        assert(table[i].right == 0);
+        table[i] = p;
+        
+        fprintf(stdout, "%u: %.*s -> [%.*s, %.*s]",
+                index_count,
                 (int) index.size, index.start,
                 (int) left.size, left.start,
                 (int) right.size, right.start);
+        fprintf(stdout, "   (%u -> [%u, %u])", i, p.left, p.right);
+        fprintf(stdout, "\n");
 
-        line_count++;
+        index_count++;
         Advance(&parser);
     }
 }
