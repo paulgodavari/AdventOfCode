b6375fb22b590c808260a8e84636090e32df544c
Paul Godavari <paulgod@gmail.com>
2023-12-08 14:44:38 -0600

    Day 08: complete part 2

--- AdventOfCode/day_08.cpp
+++ AdventOfCode/day_08.cpp
@@ -6,10 +6,20 @@
 
 #include "advent_of_code.h"
 
+// static const char* input_file_name = "day_08.test_input";  // Part 1: 2
+// static const char* input_file_name = "day_08.test_input2";  // Part 1: 6
+// static const char* input_file_name = "day_08.test_input3";  // Part 2:
+static const char* input_file_name = "day_08.input";  // Part 1: 19199, Part 2: 13663968099527
 
-static const char* input_file_name = "day_08.test_input";  // Part 1: 2, Part 2:
-// static const char* input_file_name = "day_08.test_input2";  // Part 1: 6, Part 2:
-// static const char* input_file_name = "day_08.input";  // Part 1: 19199, Part 2:
+// Part 2 computes 14895418084045582289, need to find the least common multiple of all steps:
+//    0: Steps: 13939
+//    1: Steps: 17621
+//    2: Steps: 11309
+//    3: Steps: 20777
+//    4: Steps: 19199
+//    5: Steps: 15517
+//
+//    LCM: 13663968099527
 
 
 static const u32 kMaxIndexValue = 26*26*26;
@@ -37,22 +47,25 @@ u32 StringToIndex(String str)
 
 void Day08()
 {
-    fprintf(stdout, "Day 08\n");
-    
     File input_file = ReadFile(input_file_name);
     if (!input_file.handle) {
         fprintf(stderr, "Error reading: %s\n", input_file_name);
         return;
     }
     
+    u32 index_count = 0;
     IndexPair table[kMaxIndexValue] = {};
+    
+    u32 a_count = 0;
+    u32 table_a[10] = {};
+    u32 z_count = 0;
+    u32 table_z[10] = {};
     
     ParseState parser = { input_file.data, input_file.size, 0 };
     
     String directions = ParseWord(&parser);
     Advance(&parser, 2);
     
-    u32 index_count = 0;
     while (!AtEndOfFile(&parser)) {
         String index = ParseWord(&parser);
         assert(index.size == 3);
@@ -71,6 +84,16 @@ void Day08()
         assert(table[i].right == 0);
         table[i] = p;
         
+        if (index.start[2] == 'A') {
+            table_a[a_count] = i;
+            a_count++;
+            // fprintf(stdout, "a: %.*s\n", (int) index.size, index.start);
+        } else if (index.start[2] == 'Z') {
+            table_z[z_count] = i;
+            z_count++;
+            // fprintf(stdout, "z: %.*s\n", (int) index.size, index.start);
+        }
+        
 //        fprintf(stdout, "%u: %.*s -> [%.*s, %.*s]",
 //                index_count,
 //                (int) index.size, index.start,
@@ -83,10 +106,12 @@ void Day08()
         Advance(&parser);
     }
 
+    u64 steps = 0;
+
+    // Part 1: Steps from AAA to ZZZ.
     u32 current_index = StringToIndex(CONST_STRING("AAA"));
     u32 final_index = StringToIndex(CONST_STRING("ZZZ"));
-    u32 steps = 0;
-    
+
     bool done = false;
     while (!done) {
         for (int i = 0; i < directions.size; ++i) {
@@ -104,11 +129,50 @@ void Day08()
         }
         
         if (!done) {
-            fprintf(stdout, "Restarting directions\n");
+            // fprintf(stdout, "Restarting directions\n");
+        }
+    }
+
+    fprintf(stdout, "Part 1 steps: %llu\n", steps);
+
+    // Part 2: All indices ending in 'A' to all ending in 'Z'.
+    u64 steps_to_z[10] = {};
+    
+    for (int a = 0; a < a_count; ++a) {
+        steps = 0;
+        current_index = table_a[a];
+        bool done = false;
+        while (!done) {
+            bool found_z = false;
+            for (int i = 0; i < directions.size; ++i) {
+                if (directions.start[i] == 'L') {
+                    current_index = table[current_index].left;
+                } else {
+                    current_index = table[current_index].right;
+                }
+                steps++;
+                for (int z = 0; z < a_count; ++z) {
+                    if (current_index == table_z[z]) {
+                        fprintf(stdout, "%d: Steps: %llu\n", a, steps);
+                        steps_to_z[a] = steps;
+                        found_z = true;
+                        break;
+                    }
+                }
+                if (found_z) {
+                    done = true;
+                    break;
+                }
+            }
         }
     }
     
-    fprintf(stdout, "Steps: %u\n", steps);
+    u64 steps_required = 1;
+    for (int s = 0; s < a_count; ++s) {
+        steps_required *= steps_to_z[s];
+    }
+
+    fprintf(stdout, "Part 2 steps: %llu\n", steps_required);
 }
 
 
