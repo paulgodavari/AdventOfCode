fb5f602df1069341bf204d0e7184c5beadd654f8
Paul Godavari <paulgod@gmail.com>
2023-12-15 10:14:18 -0600

    Day 15: complete part 1

--- AdventOfCode/day_15.cpp
+++ AdventOfCode/day_15.cpp
@@ -8,12 +8,18 @@
 
 
 static const char* input_file_name = "day_15.test_input";  // Part 1: 1320, part2:
-//static const char* input_file_name = "day_15.input";  // Part 1: , part2:
+// static const char* input_file_name = "day_15.input";  // Part 1: 512797, part2:
 
 
 u32 ComputeHash(String word)
 {
     u32 result = 0;
+    
+    for (int i = 0; i < word.size; ++i) {
+        result += (u32) word.start[i];
+        result *= 17;
+        result = result % 256;
+    }
     
     return result;
 }
@@ -41,8 +47,8 @@ void Day15()
             case ',':
             case '\n': {
                 String word = { word_start, word_length };
-                fprintf(stdout, "Found word: %.*s\n", (int) word.size, word.start);
                 u32 hash = ComputeHash(word);
+                fprintf(stdout, "Found word: %.*s -> %u\n", (int) word.size, word.start, hash);
                 part_1_sum += hash;
                 word_start = nullptr;
                 word_length = 0;
