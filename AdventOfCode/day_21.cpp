695caefc3bfa591c1571c58f0b31978324656f06
Paul Godavari <paulgod@gmail.com>
2023-12-21 10:25:20 -0600

    Day 21: parse input, find start position and max dimensions

--- AdventOfCode/day_21.cpp
+++ AdventOfCode/day_21.cpp
@@ -7,11 +7,21 @@
 #include "advent_of_code.h"
 
 
-static const char* input_file_name = "day_21.test_input";  // Part 1: 16, part 2:
+//static const char* input_file_name = "day_21.test_input";  // Part 1: 16, part 2:
 static const u32 kMaxSteps = 6;
 
-// static const char* input_file_name = "day_21.input";  // Part 1: , part 2:
+static const char* input_file_name = "day_21.input";  // Part 1: , part 2:
 // static const u32 kMaxSteps = 64;
+
+static const u32 kMaxRows = 131;
+static const u32 kMaxCols = 131;
+
+
+struct Position
+{
+    i32 row;
+    i32 col;
+};
 
 
 void Day21()
@@ -32,11 +42,37 @@ void Day21()
     
     // Part 1
     u64 start_time = TimeNow();
+    
+    // Find grid size and starting position.
+
+    Position dimensions = {};
+    Position start_pos = {};
+    Position current_pos = {};
+    while (!AtEndOfFile(&parser)) {
+        current_pos.col = 0;
+        while (!AtEndOfLine(&parser)) {
+            char current = parser.data[parser.offset];
+            if (current == 'S') {
+                start_pos = current_pos;
+            }
+            Advance(&parser);
+            current_pos.col++;
+        }
+        Advance(&parser);
+        current_pos.col++;
+        current_pos.row++;
+        if (dimensions.col < current_pos.col) {
+            dimensions.col = current_pos.col;
+        }
+    }
+    
+    dimensions.row = current_pos.row;
+    fprintf(stdout, "Start position: r: %u, c: %u\n", start_pos.row, start_pos.col);
+    fprintf(stdout, "Dimensions: r: %u, c: %u\n", dimensions.row, dimensions.col);
 
     fprintf(stdout, "Run time: %.4f ms\n", MillisecondsSince(run_time));
     
     CloseFile(&input_file);
-
 }
 
 
