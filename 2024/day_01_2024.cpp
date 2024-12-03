e986287e9c98b6b2c5ed228eb96364d4e8d7a6bb
Paul Godavari <paulgod@gmail.com>
2024-12-03 15:16:41 -0600

    2024 Day 01 part 2

--- 2024/day_01_2024.cpp
+++ 2024/day_01_2024.cpp
@@ -7,10 +7,11 @@
 #include "advent_of_code.h"
 
 #include <vector>
+#include <unordered_map>
 
 
-// static const char* input_file_name = "../../2024/input/day_01.test_input";  // Part 1:   Value =  11
-static const char* input_file_name = "../../2024/input/day_01.input";  // Part 1: Value = 1830467
+//static const char* input_file_name = "../../2024/input/day_01.test_input";  // Part 1:   Value =  11, Part 2:  Value = 31
+static const char* input_file_name = "../../2024/input/day_01.input";  // Part 1: Value = 1830467, Part 2:  Value = 26674158
 
 
 void Day01_2024()
@@ -34,18 +35,39 @@ void Day01_2024()
         i32 i2 = ParseNumber(&parser);
         v2.push_back(i2);
         Advance(&parser);
-        fprintf(stdout, "%u: %u\n", i1, i2);
+        //fprintf(stdout, "%u: %u\n", i1, i2);
     }
     
     std::sort(v1.begin(), v1.end());
     std::sort(v2.begin(), v2.end());
     
-    u64 distance = 0;
+    u64 part1_distance = 0;
     for (int index = 0; index < v1.size(); ++index) {
-        distance += abs(v1[index] - v2[index]);
+        part1_distance += abs(v1[index] - v2[index]);
     }
     
-    fprintf(stdout, "2024: Day 01 part 1: %llu\n", distance);
+    std::unordered_map<u32, u32> h2;
+    for (int index = 0; index < v2.size(); ++index) {
+        i32 value = v2[index];
+        auto it = h2.find(value);
+        if (it != h2.end()) {
+            h2[value] = it->second + 1;
+        } else {
+            h2[value] = 1;
+        }
+    }
+    
+    u64 part2_distance = 0;
+    for (int index = 0; index < v1.size(); ++index) {
+        u32 current = v1[index];
+        auto it = h2.find(current);
+        if (it != h2.end()) {
+            part2_distance += current * it->second;
+        }
+    }
+    
+    fprintf(stdout, "2024: Day 01 part 1: %llu\n", part1_distance);
+    fprintf(stdout, "2024: Day 01 part 2: %llu\n", part2_distance);
     fprintf(stdout, "Total time: %.4f ms\n", MillisecondsSince(run_time_start));
 }
 
