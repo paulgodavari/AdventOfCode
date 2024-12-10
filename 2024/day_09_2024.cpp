162f216e7d619c3b64ffef2f387c0259e172fa95
Paul Godavari <paulgod@gmail.com>
2024-12-10 15:42:12 -0600

    2024 Day 09 Part 2

--- 2024/day_09_2024.cpp
+++ 2024/day_09_2024.cpp
@@ -8,8 +8,8 @@
 
 #include <vector>
 
-static const char* input_file_name = "../../2024/input/day_09.test_input";  // Part 1 = 1928, Part 2 =
-// static const char* input_file_name = "../../2024/input/day_09.input";  // Part 1 = 6353658451014, Part 2 =
+// static const char* input_file_name = "../../2024/input/day_09.test_input";  // Part 1 = 1928, Part 2 = 2858
+static const char* input_file_name = "../../2024/input/day_09.input";  // Part 1 = 6353658451014, Part 2 = 6382582136592
 
 
 enum class BlockType : u16
@@ -23,6 +23,15 @@ struct Block
 {
     BlockType type;
     u16 id;
+};
+
+
+struct Span
+{
+    BlockType type;
+    u16 id;
+    u32 index;
+    u32 size;
 };
 
 
@@ -80,6 +89,24 @@ void PrintBlocks(const std::vector<Block>& blocks)
 }
 
 
+void PrintSpan(const std::vector<Span>& spans)
+{
+    for (int i = 0; i < spans.size(); ++i) {
+        Span span = spans[i];
+        if (span.type == BlockType::Space) {
+            for (int j = 0; j < span.size; ++j) {
+                fprintf(stdout, ".");
+            }
+        } else {
+            for (int j = 0; j < span.size; ++j) {
+                fprintf(stdout, "%x", span.id);
+            }
+        }
+    }
+    fprintf(stdout, "\n");
+}
+
+
 void Day09_2024()
 {
     u64 run_time_start = TimeNow();
@@ -93,7 +120,6 @@ void Day09_2024()
     ParseState parser = { input_file.data, input_file.size, 0 };
     
     u64 part1_answer = 0;
-    u64 part2_answer = 0;
     
     u32 id_count = 0;
     u32 space_count = 0;
@@ -140,6 +166,56 @@ void Day09_2024()
     for (int index = 0; index < blocks.size(); ++index) {
         if (blocks[index].type == BlockType::File) {
             part1_answer += index * blocks[index].id;
+        }
+    }
+    
+    // Part 2. Reset everything and re-parse.
+    parser.offset = 0;
+    is_file = true;
+    id_count = 0;
+
+    u32 index = 0;
+    std::vector<Span> files;
+    std::vector<Span> spaces;
+
+    while (!AtEndOfLine(&parser)) {
+        u32 digit = (u32) (parser.data[parser.offset] - '0');
+        if (is_file) {
+            Span span = { BlockType::File, (u16) id_count, index, digit };
+            files.push_back(span);
+            id_count++;
+        } else {
+            Span span = { BlockType::Space, 0, index, digit };
+            spaces.push_back(span);
+        }
+        is_file = !is_file;
+        parser.offset++;
+        index += digit;
+    }
+
+    // PrintSpan(spans);
+    
+    // Try to find the first that fits for each file, last file first.
+    for (int i = files.size() - 1; i > 0; --i) {
+        Span& file = files[i];
+        for (int j = 0; j < spaces.size(); ++j) {
+            Span& space = spaces[j];
+            if (space.index < file.index && space.size >= file.size) {
+                // We can move the file into the space.
+                file.index = space.index;
+                space.size -= file.size;
+                space.index += file.size;
+                break;
+            }
+        }
+    }
+    
+    // Compute checksum
+    u64 part2_answer = 0;
+    for (int i = 0; i < files.size(); ++i) {
+        Span file = files[i];
+        for (int j = 0; j < file.size; ++j) {
+            part2_answer += file.id * (file.index + j);
         }
     }
 
