91d07e5db5e618a05c32cb4897194889174f5857
Paul Godavari <paulgod@gmail.com>
2025-12-02 11:10:26 -0600

    2025 Day 01 Part 2

--- 2025/day_01_2025.cpp
+++ 2025/day_01_2025.cpp
@@ -6,8 +6,9 @@
 
 #include "advent_of_code.h"
 
-// static const char* input_file_name = "../../2025/input/day_01.test_input";  // Part 1 = 3, Part 2 = 
-static const char* input_file_name = "../../2025/input/day_01.input";  // Part 1 = 1152, Part 2 =
+
+// static const char* input_file_name = "../../2025/input/day_01.test_input";  // Part 1 = 3, Part 2 = 6
+static const char* input_file_name = "../../2025/input/day_01.input";  // Part 1 = 1152, Part 2 = 6671
 
 
 void Day01_2025()
@@ -19,8 +20,6 @@ void Day01_2025()
         fprintf(stderr, "Error reading %s\n", input_file_name);
         return;
     }
-    
-    ParseState parser = { input_file.data, input_file.size, 0 };
     
     u64 part1_answer = 0;
     u64 part2_answer = 0;
@@ -34,11 +33,31 @@ void Day01_2025()
     while (fscanf(input_file.handle, " %c%d", &direction, &steps) == 2) {
         if (direction == 'L' || direction == 'R') {
             int sign = direction == 'L' ? -1 : 1;
+            
+            // Calculate the new position.
             int new_position = (position + sign * steps) % 100;
-            position = new_position < 0 ? (100 + new_position) : new_position;
-            if (position == 0) {
-                part1_answer++;
+            if (new_position < 0) {
+                new_position += 100;
             }
+            
+            // Part 1 & 2: count the number of times we end up at zero.
+            if (new_position == 0) {
+                part1_answer++;
+                part2_answer++;
+            }
+            
+            // Part 2: we pass zero for each full rotation.
+            int full_rotations = steps / 100;
+            part2_answer += full_rotations;
+            
+            // Part 2: if our new position is outside the range 0..99, we've passed zero,
+            //         excluding the case where we start at zero and go left.
+            int unadjusted_position = position + (steps % 100) * sign;
+            if ((unadjusted_position < 0 && position != 0) || (unadjusted_position > 100)) {
+                part2_answer++;
+            }
+
+            position = new_position;
         } else {
             fprintf(stderr, "Bad direction: %d\n", direction);
             break;
